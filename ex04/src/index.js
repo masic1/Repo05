cb85a6639e8fa34446f80a774029412bfac6303c
masic1 <masic.mirza@gmail.com>
2021-09-18 09:51:33 +0200

    Added exercises 1-20

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    var stringOne = "This is the first string. "
+    var stringTwo = "This is the second string."
+
+    var myString = stringOne + stringTwo;
+    // Only change code above this line
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
+
+
---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "I had a small" + " " + myNoun + "." + " " + "He" + " " + myVerb + " " + myAdverb + " " + "around the house because he wasn't that" + " " + myAdjective + ".";
+    // Only change code above this line
+    return wordBlanks;    
+}
+
+console.log(main());
+module.exports = main;
